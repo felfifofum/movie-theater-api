a4cbf067466ba30651da15cb54c293cee43e7ce1
Felicity Bruce <felgbru@gmail.com>
2022-11-11 18:35:57 +0000

    Basic middleware function logger

---
+++ middleware/logger.js
@@ -0,0 +1,8 @@
+//middleware function
+async function logger(req, res, next) {
+  console.log(`${req.method} ${req.originalUrl} at ${new Date()}`);
+
+  next();
+}
+
+module.exports = logger;
--- server.js
+++ server.js
@@ -2,13 +2,19 @@ const express = require("express");
 const { db } = require("./db");
 const userRouter = require("./routes/user");
 const showRouter = require(`./routes/shows`);
+const logger = require(`./middleware/logger`)
 const seed = require("./seed");
 
 const app = express();
 app.use(express.json());
 
+//logger middleware-runs every time http request occurs-has to be nefore ofther 'use'
+app.use(logger)
+
+
 app.use(`/users`, userRouter);
 app.use(`/shows`, showRouter);
+
 
 const HOST = process.env["HOST"] || "localhost";
 const PORT = process.env["PORT"] || 3000;
